fe671084ee7c72e5640614ad730adb803b112961
Brahmam-12 <98756307+Brahmam-12@users.noreply.github.com>
2026-09-09 14:22:12 +0530

    pipeline updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_service_plan" "asp" {
 
 resource "azurerm_linux_web_app" "webapp" {
   name = var.web_app_name
-  location = "Central Canada"
+  location = "Canada Central"
   service_plan_id = azurerm_service_plan.asp.id
   resource_group_name = azurerm_resource_group.rsg.name
   site_config {
